1a3b3d0c62912afb4121b29e1ff20a1a66b9fa5f
treenidad <ktrinidad209@gmail.com>
2025-02-15 13:52:00 -0800

    Initial commit

--- portfolio/src/components/Contacts.js
+++ portfolio/src/components/Contacts.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contacts = () => {
-  return <div>contact</div>;
+  return <div>Contact</div>;
 };
 
 export default Contacts;
--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [listNav] = useState(["home", "skills", "project", "contacts"]);
+  return (
+    <header>
+      <div className="logo.png">
+        <img src="/logo512.png" alt="" />
+      </div>
+      <nav>
+        {listNav.map((value, key) => (
+          <span key={key}>{value}</span>
+        ))}
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
