978be7b90c255e7b87e89d876925f1e2f3aa0221
treenidad <ktrinidad209@gmail.com>
2025-05-06 18:22:27 -0700

    WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="content">
+        <div className="name">
+          MY NAME IS <span>Kevin Trinidad</span>
+        </div>
+        <div className="des">
+          lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <a href="/democv.pdf" target="_blank" rel="noopeneer noreferrer">
+          Download My CV
+        </a>
+      </div>
+      <div className="avatar">
+        <div className="card">
+          <img src="/profile-pic.jpg" alt="profile-pic" />
+          <div className="info">
+            <div>Developer</div>
+            <div>Filipino</div>
+            <div>12/18</div>
+            <div>Male</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { FortAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faReact,
+  faHtml5,
+  faCss3,
+  faJs,
+  faVuejs,
+  faLaravel,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Skills = () => {
-  return <div>Skill</div>;
+  const [listSkills] = useState([
+    {
+      name: "HTML",
+      des: "Lorem ipsum",
+      icon: faHtml5,
+    },
+    {
+      name: "CSS",
+      des: "Lorem ipsum",
+      icon: faCss3,
+    },
+    {
+      name: "JS",
+      des: "Lorem ipsum",
+      icon: faJs,
+    },
+    {
+      name: "React",
+      des: "Lorem ipsum",
+      icon: faReact,
+    },
+    {
+      name: "VueJS",
+      des: "Lorem ispum",
+      icon: faVuejs,
+    },
+    {
+      name: "Laravel",
+      des: "Lorem ipsum",
+      icon: faLaravel,
+    },
+  ]);
+  return (
+    <section className="skills">
+      Skill
+      <div className="title">These are my skills</div>
+      <div className="des">Lorem ipsum</div>
+      <div className="list"></div>
+    </section>
+  );
 };
 
 export default Skills;
